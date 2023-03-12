049293bb4d429e06fb3ce38b4c1be87bcabcdb4a
qiqihaobenben <chenfangxu_qixin@163.com>
2023-03-13 07:07:56 +0800

    docs: 更新内存相关

--- test/demo.js
+++ test/demo.js
@@ -1,11 +1,25 @@
 'use strict'
 
-const set = new Set([1, 'some time', 2, 4, 6, 2, 2, 1, 5, 3])
-
-console.log([...set])
-
-function eliminateDuplicates(items) {
-  return [...new Set(items)]
+function testCatch1() {
+  let catch1 = 1
+  var catch2 = 'str'
+  const catch3 = true
+  let catch4 = { a: 1 }
+  return function() {
+    console.log(catch1, catch2, catch3, catch4)
+  }
 }
 
-console.log(eliminateDuplicates(set))
+function testCatch2() {
+  let catch1 = 1
+  let catch2 = 'str'
+  let catch3 = true
+  var catch4 = { a: 1 }
+  return class {
+    constructor() {
+      console.log(catch1, catch2, catch3, catch4)
+    }
+  }
+}
+console.dir(testCatch1())
+console.dir(testCatch2())
