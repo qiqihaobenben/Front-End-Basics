fbfd1bb641a177d586067c6bf6cdd4c43fbb0131
qiqihaobenben <chenfangxu_qixin@163.com>
2023-03-14 07:11:22 +0800

    docs: 更新内存相关

--- test/demo.js
+++ test/demo.js
@@ -1,25 +1,31 @@
 'use strict'
 
-function testCatch1() {
-  let catch1 = 1
-  var catch2 = 'str'
-  const catch3 = true
-  let catch4 = { a: 1 }
-  return function() {
-    console.log(catch1, catch2, catch3, catch4)
-  }
-}
+// function testCatch1() {
+//   let catch1 = 1
+//   var catch2 = 'str'
+//   const catch3 = true
+//   let catch4 = { a: 1 }
+//   return function() {
+//     console.log(catch1, catch2, catch3, catch4)
+//   }
+// }
 
-function testCatch2() {
-  let catch1 = 1
-  let catch2 = 'str'
-  let catch3 = true
-  var catch4 = { a: 1 }
-  return class {
-    constructor() {
-      console.log(catch1, catch2, catch3, catch4)
-    }
-  }
+// function testCatch2() {
+//   let catch1 = 1
+//   let catch2 = 'str'
+//   let catch3 = true
+//   var catch4 = { a: 1 }
+//   return class {
+//     constructor() {
+//       console.log(catch1, catch2, catch3, catch4)
+//     }
+//   }
+// }
+// console.dir(testCatch1())
+// console.dir(testCatch2())
+
+function a() {
+  let b = 1
+  debugger
 }
-console.dir(testCatch1())
-console.dir(testCatch2())
+a()
