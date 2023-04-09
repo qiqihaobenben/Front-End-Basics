3e4b5f164dcf4f57a29c6694be3d330e48342d66
qiqihaobenben <chenfangxu_qixin@163.com>
2023-04-09 14:23:53 +0800

    docs: 更新 JavaScript 内存

--- test/demo.js
+++ test/demo.js
@@ -1,31 +1,20 @@
-'use strict'
-
-// function testCatch1() {
-//   let catch1 = 1
-//   var catch2 = 'str'
-//   const catch3 = true
-//   let catch4 = { a: 1 }
-//   return function() {
-//     console.log(catch1, catch2, catch3, catch4)
-//   }
-// }
-
-// function testCatch2() {
-//   let catch1 = 1
-//   let catch2 = 'str'
-//   let catch3 = true
-//   var catch4 = { a: 1 }
-//   return class {
-//     constructor() {
-//       console.log(catch1, catch2, catch3, catch4)
-//     }
-//   }
-// }
-// console.dir(testCatch1())
-// console.dir(testCatch2())
-
-function a() {
-  let b = 1
-  debugger
+function TestClass() {
+  this.number = 123
+  this.string = 'abc'
+  this.boolean = true
+  this.symbol = Symbol('test')
+  this.undefined = undefined
+  this.null = null
+  this.object = { name: 'pp' }
+  this.array = [1, 2, 3]
+  this.getSet = {
+    _value: 0,
+    get value() {
+      return this._value
+    },
+    set value(v) {
+      this._value = v
+    },
+  }
 }
-a()
+let testObject = new TestClass()
